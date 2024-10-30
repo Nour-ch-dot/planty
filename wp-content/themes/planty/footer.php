a3b68e2de3ed488413173eba123a321dfb78590a
Nour-ch-dot <nourelhoudachetouh@gmail.com>
2024-10-30 13:18:37 +0100

    update theme

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,11 @@
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+    <a href="<?php echo get_home_url(); ?>/mentions-legales">Mentions Légales</a>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
